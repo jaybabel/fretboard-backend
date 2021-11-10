a038d064358c20d00804009fc73a8f8d1f71aad3
jaybabel <BabelJayE@JohnDeere.com>
2021-11-10 17:06:04 -0600

    query testing

--- postgres_scripts/queries.sql
+++ postgres_scripts/queries.sql
@@ -5,3 +5,12 @@ INNER JOIN key_chord
 INNER JOIN musicalkey
 	ON key_chord.musicalkey_id = musicalkey.id
 WHERE musicalkey.keyname = 'C';
+
+
+SELECT CONCAT (chords.chordname,'+') 
+FROM chords 
+INNER JOIN key_chord
+	ON key_chord.chord_id = chords.id
+INNER JOIN musicalkey
+	ON key_chord.musicalkey_id = musicalkey.id
+WHERE musicalkey.keyname = 'E';
