6d13e740a890db8e9274df7797f2f226cf8ef927
jaybabel <BabelJayE@JohnDeere.com>
2021-11-11 00:26:15 -0600

    query key and scales

--- postgres_scripts/queries.sql
+++ postgres_scripts/queries.sql
@@ -14,3 +14,12 @@ INNER JOIN key_chord
 INNER JOIN musicalkey
 	ON key_chord.musicalkey_id = musicalkey.id
 WHERE musicalkey.keyname = 'E';
+
+
+SELECT scales.scalename 
+FROM scales 
+INNER JOIN key_scale
+	ON key_scale.scale_id = scales.id
+INNER JOIN musicalkey
+	ON key_scale.musicalkey_id = musicalkey.id
+WHERE musicalkey.keyname = 'A';
