bb0e0cc8e82407afec8dcc6eaf7b1a384bbf7e99
jaybabel <BabelJayE@JohnDeere.com>
2021-11-11 23:18:57 -0600

    Created seeder files

--- postgres_scripts/populate-chords-table.sql
+++ postgres_scripts/populate-chords-table.sql
@@ -1,143 +1,143 @@
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (1, 'C', './public/C_open.png', false);
+	VALUES (1, 'C', 'C_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (2, 'G', './public/G_open.png', false);
+	VALUES (2, 'G', 'G_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (3, 'D', './public/D_open.png', false);
+	VALUES (3, 'D', 'D_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (4, 'A', './public/A_open.png', false);
+	VALUES (4, 'A', 'A_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (5, 'E', './public/E_open.png', false);
+	VALUES (5, 'E', 'E_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
 	VALUES (6, 'B', '/public/B_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (7, 'F#', './public/F#_open.png', false);
+	VALUES (7, 'F#', 'F#_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (8, 'Ab', './public/Ab_open.png', false);
+	VALUES (8, 'Ab', 'Ab_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (9, 'Ab', './public/Ab_open.png', false);
+	VALUES (9, 'Ab', 'Ab_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (10, 'Eb', './public/Eb_open.png', false);
+	VALUES (10, 'Eb', 'Eb_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (11, 'Bb', './public/Bb_open.png', false);
+	VALUES (11, 'Bb', 'Bb_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (12, 'F', './public/F_open.png', false);
+	VALUES (12, 'F', 'F_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (13, 'Am', './public/Am_open.png', false);
+	VALUES (13, 'Am', 'Am_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (14, 'Em', './public/Em_open.png', false);
+	VALUES (14, 'Em', 'Em_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (15, 'Bm', './public/Bm_open.png', false);
+	VALUES (15, 'Bm', 'Bm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (16, 'F#m', './public/F#m_open.png', false);
+	VALUES (16, 'F#m', 'F#m_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (17, 'C#m', './public/C#m_open.png', false);
+	VALUES (17, 'C#m', 'C#m_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (18, 'G#m', './public/G#m_open.png', false);
+	VALUES (18, 'G#m', 'G#m_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (19, 'Ebm', './public/Ebm_open.png', false);
+	VALUES (19, 'Ebm', 'Ebm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (20, 'Bbm', './public/Bbm_open.png', false);
+	VALUES (20, 'Bbm', 'Bbm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (21, 'Fm', './public/Fm_open.png', false);
+	VALUES (21, 'Fm', 'Fm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (22, 'Cm', './public/Cm_open.png', false);
+	VALUES (22, 'Cm', 'Cm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (23, 'Gm', './public/Gm_open.png', false);
+	VALUES (23, 'Gm', 'Gm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (24, 'Dm', './public/Dm_open.png', false);
+	VALUES (24, 'Dm', 'Dm_open.png', false);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (25, 'C', './public/C_bar.png', true);
+	VALUES (25, 'C', 'C_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (26, 'G', './public/G_bar.png', true);
+	VALUES (26, 'G', 'G_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (27, 'D', './public/D_bar.png', true);
+	VALUES (27, 'D', 'D_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (28, 'A', './public/A_bar.png', true);
+	VALUES (28, 'A', 'A_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (29, 'E', './public/E_bar.png', true);
+	VALUES (29, 'E', 'E_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (30, 'B', './public/B_bar.png', true);
+	VALUES (30, 'B', 'B_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (31, 'F#', './public/F#_bar.png', true);
+	VALUES (31, 'F#', 'F#_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (32, 'Db', './public/Db_bar.png', true);
+	VALUES (32, 'Db', 'Db_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (33, 'Ab', './public/Ab_bar.png', true);
+	VALUES (33, 'Ab', 'Ab_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (34, 'Eb', './public/Eb_bar.png', true);
+	VALUES (34, 'Eb', 'Eb_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (35, 'Bb', './public/Bb_bar.png', true);
+	VALUES (35, 'Bb', 'Bb_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (36, 'F', './public/F_bar.png', true);
+	VALUES (36, 'F', 'F_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (37, 'Am', './public/Am_bar.png', true);
+	VALUES (37, 'Am', 'Am_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (38, 'Em', './public/Em_bar.png', true);
+	VALUES (38, 'Em', 'Em_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (39, 'Bm', './public/Bm_bar.png', true);
+	VALUES (39, 'Bm', 'Bm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (40, 'F#m', './public/F#m_bar.png', true);
+	VALUES (40, 'F#m', 'F#m_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (41, 'C#m', './public/C#m_bar.png', true);
+	VALUES (41, 'C#m', 'C#m_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (42, 'G#m', './public/G#m_bar.png', true);
+	VALUES (42, 'G#m', 'G#m_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (43, 'Ebm', './public/Ebm_bar.png', true);
+	VALUES (43, 'Ebm', 'Ebm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (44, 'Bbm', './public/Bbm_bar.png', true);
+	VALUES (44, 'Bbm', 'Bbm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (45, 'Fm', './public/Fm_bar.png', true);
+	VALUES (45, 'Fm', 'Fm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (46, 'Cm', './public/Cm_bar.png', true);
+	VALUES (46, 'Cm', 'Cm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (47, 'Gm', './public/Gm_bar.png', true);
+	VALUES (47, 'Gm', 'Gm_bar.png', true);
 
 INSERT INTO chords (id, chordname, imageurl, isbarchord)
-	VALUES (48, 'Dm', './public/Dm_bar.png', true);
+	VALUES (48, 'Dm', 'Dm_bar.png', true);
