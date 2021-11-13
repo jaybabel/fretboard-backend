abfe81b3fac963fa3ac00914f4d39755d68835a8
jaybabel <BabelJayE@JohnDeere.com>
2021-11-12 19:31:53 -0600

    route to MusicalKeys.findAll is working

--- models/scales.js
+++ models/scales.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Scales.hasMany(models.MusicalKey, { through: models.Key_Scales });
+      // Scales.hasMany(models.MusicalKey, { through: models.Key_Scales });
     }
   };
   Scales.init({
