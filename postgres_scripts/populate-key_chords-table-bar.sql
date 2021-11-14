5caa2c440f994ef083fef556558382ae22f37aa2
jaybabel <BabelJayE@JohnDeere.com>
2021-11-14 11:08:07 -0600

    added sql scripts to populate MusicalKeyId and ChordId in Key_Chords table

---
+++ postgres_scripts/populate-key_chords-table-bar.sql
@@ -0,0 +1,83 @@
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 25);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 26);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 35);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 37);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 38);
+INSERT INTO"Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 48);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 25);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 26);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 27);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 37);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 38);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 39);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 26);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 27);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 28);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 38);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 39);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 40);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 27);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 28);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 29);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 39);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 40);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 41);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 28);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 29);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 30);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 40);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 41);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 42);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 29);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 30);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 31);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 41);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 42);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 43);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 30);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 31);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 32);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 42);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 43);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 44);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 31);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 32);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 33);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 43);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 44);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 45);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 32);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 33);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 34);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 44);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 45);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 46);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 33);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 34);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 35);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 45);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 46);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 47);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 34);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 35);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 36);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 46);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 47);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 48);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 35);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 36);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 25);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 47);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 48);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 37);
---
+++ postgres_scripts/populate-key_chords-table-open.sql
@@ -0,0 +1,83 @@
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 1);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 2);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 12);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 13);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 14);
+INSERT INTO"Key_Chords" ("MusicalKeyId", "ChordId") VALUES (1, 24);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 1);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 2);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 3);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 13);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 14);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (2, 15);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 2);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 3);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 4);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 14);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 15);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (3, 16);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 3);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 4);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 5);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 15);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 16);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (4, 17);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 4);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 5);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 6);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 16);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 17);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (5, 18);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 5);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 6);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 7);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 17);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 18);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (6, 19);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 6);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 7);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 8);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 18);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 19);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (7, 20);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 7);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 8);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 9);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 19);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 20);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (8, 21);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 8);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 9);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 10);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 20);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 21);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (9, 22);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 9);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 10);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 11);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 21);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 22);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (10, 23);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 10);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 11);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 12);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 22);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 23);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (11, 24);
+
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 11);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 12);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 1);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 23);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 24);
+INSERT INTO "Key_Chords" ("MusicalKeyId", "ChordId") VALUES (12, 13);
