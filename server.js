e3ccb806d35b6bc0ef5185b9f092d4e40a7e2bc4
jaybabel <BabelJayE@JohnDeere.com>
2021-11-14 22:04:38 -0600

    added code to server.js and ran npm install cors to fix cors browser issue

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 const methodOverride = require('method-override')
 const routes = require('./routes');
 
+// cors issue
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+// end cors code
+
 //Middleware
 // app.use(cookieParser());
 app.use(express.static("public"));
