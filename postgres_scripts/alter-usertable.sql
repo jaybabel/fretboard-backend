d234ba3bd1bbefe058e94677ad6a0c3dfc26ab06
jaybabel <BabelJayE@JohnDeere.com>
2021-11-18 09:32:30 -0600

    added sql file with alterations to the User_Accounts table

---
+++ postgres_scripts/alter-usertable.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "User_Accounts"
+ADD UNIQUE (username);
+
+ALTER TABLE "User_Accounts"
+ADD COLUMN is_admin BOOLEAN;
+
+UPDATE "User_Accounts"
+SET is_admin = true
+WHERE id = 1;
+
+ALTER TABLE "User_Accounts"
+ALTER COLUMN is_admin
+SET NOT NULL;
+
+SELECT * FROM "User_Accounts";
+
--- postgres_scripts/queries.sql
+++ postgres_scripts/queries.sql
@@ -1,10 +1,21 @@
+-- SELECT "Chords".chordname
+-- FROM "Chords" 
+-- INNER JOIN "Key_Chords"
+-- 	ON "Key_Chords".chordid = "Chords".id
+-- INNER JOIN "MusicalKeys"
+-- 	ON "Key_Chords".keyid = "MusicalKeys".id
+-- WHERE "MusicalKeys".keyname = 'C';
+
+/* Updated query*/
 SELECT "Chords".chordname
 FROM "Chords" 
 INNER JOIN "Key_Chords"
-	ON "Key_Chords".chordid = "Chords".id
+	ON "Key_Chords"."ChordId" = "Chords".id
 INNER JOIN "MusicalKeys"
-	ON "Key_Chords".keyid = "MusicalKeys".id
-WHERE "MusicalKeys".keyname = 'C';
+	ON "Key_Chords"."MusicalKeyId" = "MusicalKeys".id
+WHERE "MusicalKeys".keyname = 'F#';
+
+
 
 
 SELECT CONCAT ("Chords".chordname,'+') 
