4ce8e97e3e6e2b9f35e35775d6a4581800ea9557
jaybabel <BabelJayE@JohnDeere.com>
2021-11-23 08:09:10 -0600

    Added sql statements to recordings script

--- postgres_scripts/populate-recordings-table.sql
+++ postgres_scripts/populate-recordings-table.sql
@@ -4,6 +4,11 @@ VALUES (1, 'E-A-D-A-E', './Downloads/E-A-D-A-E-progression.mp3', 2, 'Open chords
 INSERT INTO "User_Recordings" (id, recordingname, recordingurl, "userId", memo) 
 VALUES (2, 'Wish You Were Here', './Downloads/WishYouWereHere.mp3', 2, 'Opening riff to Pink Floyd Wish You Were Here.');
 
+INSERT INTO "User_Recordings" (id, recordingname, recordingurl, "userId", memo) 
+VALUES (3, 'Am-D Riders', './Downloads/Am-D-progression.mp3', 4, 'A 16 second open chords progression from Am to D. Quick tempo. Kind of a western movie feel.');
+
+
+
 
 UPDATE "User_Recordings"
 SET imageurl = 'https://fretboard-mp3s.s3.us-east-2.amazonaws.com/E-A-D-A-E-progression.mp3'
