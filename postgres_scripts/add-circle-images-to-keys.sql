43676c9b6422624548891ca80f7308195742a289
jaybabel <BabelJayE@JohnDeere.com>
2021-11-29 14:08:54 -0600

    Updated paths to circle diagrams

--- postgres_scripts/add-circle-images-to-keys.sql
+++ postgres_scripts/add-circle-images-to-keys.sql
@@ -1,36 +1,36 @@
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-C.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-C.png'
 WHERE id = 1;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-G.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-G.png'
 WHERE id = 2;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-D.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-D.png'
 WHERE id = 3;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-A.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-A.png'
 WHERE id = 4;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-E.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-E.png'
 WHERE id = 5;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-B.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-B.png'
 WHERE id = 6;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-Gb.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-Gb.png'
 WHERE id = 7;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-Db.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-Db.png'
 WHERE id = 8;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-Ab.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-Ab.png'
 WHERE id = 9;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-Eb.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-Eb.png'
 WHERE id = 10;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-Bb.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-Bb.png'
 WHERE id = 11;
 UPDATE "MusicalKeys"
-SET imageurl = 'circleof5-F.png'
+SET imageurl = 'https://fretboard-images.s3.us-east-2.amazonaws.com/circleof5-F.png'
 WHERE id = 12;
