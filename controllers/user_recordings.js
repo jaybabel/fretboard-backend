e0aaea590f4371aece7409d213c08a47acc2734e
jaybabel <BabelJayE@JohnDeere.com>
2021-12-17 15:10:30 -0600

    Added username filter to Recording Name list

--- controllers/user_recordings.js
+++ controllers/user_recordings.js
@@ -1,4 +1,6 @@
 const User_Recordings = require('../models').User_Recordings;
+const User = require('../models').User_Account;
+const { Op } = require("sequelize");
 
 // app.get('/keys', function (req, res) {
 //     res.send('musical key request')
@@ -6,7 +8,15 @@ const User_Recordings = require('../models').User_Recordings;
 
 const getRecordings = (req, res) => {
     // res.send('recordings request received in controller')
-    User_Recordings.findAll()
+    User_Recordings.findAll(
+        // add join
+        { include: [{ 
+            model: User,
+            where: {username: {[Op.eq]: 'Jay'}
+            }
+          }]
+        }
+    )   
     .then(recordings => {
         res.json(recordings)
     })
@@ -16,7 +26,7 @@ const getRecordings = (req, res) => {
 }
 
 const getRecordingData = (req, res) => {
-    console.log("getRecordingData request, ", req.body)
+   // console.log("getRecordingData request, ", req.body)
     User_Recordings.findOne({
         attributes:['recordingname', 'recordingurl', 'memo'],
         where: { recordingname: req.body.recordingname }
